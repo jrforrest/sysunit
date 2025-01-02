40f23a007c1bce94e5851aaf05e7505713bbc7b9
Jack Forrest <jack@jrforrest.net>
2025-01-02 10:42:59 -0700

    Doc CLI code a bit

--- src/cli/reporter.rs
+++ src/cli/reporter.rs
@@ -1,3 +1,13 @@
+/// Reports on events emitted from the engine to the CLI
+///
+/// There's some complexity here in that these events don't map very well
+/// to the UI. Also, we need to support capturing unit output for future
+/// printing in case an error arises, and change the output based on level
+/// of verbosity.  
+///
+/// This is handled with nested state machines, called Contexts here. Contexts
+/// are entered and exited by their parent in response to engine events. Events
+/// are delegated down the context chain until they are handled.
 use anyhow::Result;
 
 use crate::events::{Event, Observer};
@@ -10,7 +20,7 @@ pub struct EngineLogger {
     context: RootContext,
 }
 
-/// Sets the noise level for the CLI reporter
+/// Sets the noise level for a reporter
 #[derive(Clone, Debug, PartialOrd, PartialEq)]
 pub enum Verbosity {
     Quiet,
